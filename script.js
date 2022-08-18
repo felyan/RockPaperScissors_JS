bca788cbe469af96b3874aa43fe9cd41b1e1e731
Eva Szilardne Kallai <kallaiszilardne@gmail.com>
2022-08-18 16:14:40 +0200

    getResult()

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ function getComputerChoice() {
   let computerChoice = rpsChoices[Math.floor(Math.random() * 3)];
   return computerChoice;
 }
+
+// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
+
+function getResult(playerChoice, computerChoice) {
+  // return the result of score based on if you won, drew, or lost
+
+  let score;
+
+  // All situations where user draws, set `score` to 0
+  if (playerChoice === computerChoice) {
+    score = 0;
+    // All situations where user wins, set 'score' to 1
+  } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+    score = 1;
+  } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+    score = 1;
+  } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+    score = 1;
+
+    // Otherwise user loses (aka set score to -1)
+  } else {
+    score = -1;
+  }
+
+  // return score
+  return score;
+}
