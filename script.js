c0e92cd0d9a06d6511fb48515eaf75ead54c6904
Eva Szilardne Kallai <kallaiszilardne@gmail.com>
2022-08-18 16:46:19 +0200

    playGame()

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ function getResult(playerChoice, computerChoice) {
   // return score
   return score;
 }
+
+// ** Calculate who won and show it on the screen **
+function onClickRPS(playerChoice) {
+  console.log({ playerChoice });
+  const computerChoice = getComputerChoice();
+  console.log({ computerChoice });
+  const score = getResult(playerChoice, computerChoice);
+  console.log({ score });
+}
+
+// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
+function playGame() {
+  let rpsButtons = document.querySelectorAll(".rpsButton");
+
+  rpsButtons.forEach(rpsButton => {
+    rpsButton.onclick = () => onClickRPS(rpsButton);
+  });
+}
+
+playGame();
